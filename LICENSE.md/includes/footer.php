28e36dfdaabcc7a8967e6f05ccde0dbc059bc7c9
afwerki12 <61809053+afwerki12@users.noreply.github.com>
2020-04-19 08:52:34 -0700

    Add files via upload

---
+++ LICENSE.md/includes/footer.php
@@ -0,0 +1,21 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/afework.grumnhe.1?epa=SEARCH_BOX"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://www.instagram.com/haylemareyam_ag/"><span>Twitter</span></a></li>
+				<!--<li><a class="flickr" href="#"><span>Flickr</span></a></li>
+				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+				<li><a class="dribbble" href="#"><span>Dribbble</span></a></li>-->
+			</ul>
+		</div>
+		
+	</div>
+</div>
