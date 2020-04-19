3aeacd83483ff05f790371757ef57db6a242ccc2
afwerki12 <61809053+afwerki12@users.noreply.github.com>
2020-04-19 09:04:49 -0700

    Delete footer.php

--- LICENSE.md/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="copyrights">
-	 <p>© 2020. this website is created for education perpose |  <a href="https://www.facebook.com/afework.grumnhe.1?epa=SEARCH_BOX">afwerki ghirmay</a> </p>
-</div>	
