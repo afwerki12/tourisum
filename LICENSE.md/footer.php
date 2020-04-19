f3655a69adf153888bc46d3e83dc938efe8f27ec
afwerki12 <61809053+afwerki12@users.noreply.github.com>
2020-04-19 08:29:13 -0700

    Add files via upload

---
+++ LICENSE.md/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2020. this website is created for education perpose |  <a href="https://www.facebook.com/afework.grumnhe.1?epa=SEARCH_BOX">afwerki ghirmay</a> </p>
+</div>	
